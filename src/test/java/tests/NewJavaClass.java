3f71bb9cae34844065b60c217f7bf8274e4182a3
okulovmaxim <okulov.m.i@gmail.com>
2024-11-30 13:10:24 +0500

    Добавил новый класс

---
+++ src/test/java/tests/NewJavaClass.java
@@ -0,0 +1,12 @@
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class NewJavaClass {
+
+    @Test
+    public void simpleTest() {
+        Assert.assertTrue(1==1);
+    }
+}
